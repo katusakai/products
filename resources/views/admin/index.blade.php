82d8674dc63ede13dc41becf1be1e8aed40fcf01
TadasJanca <tadasjanca@gmail.com>
2019-07-16 08:06:39 +0300

    minor cleanup

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,20 +1,6 @@
 @extends('layouts/app')
 
 @section('content')
-{{--    <div class="container">--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-md-8">--}}
-{{--                <admin-products-table-component--}}
-{{--                        route-index="{{route('admin.products.index')}}"--}}
-{{--                        page="{{isset($_GET['page']) ? $_GET['page'] : 1}}"--}}
-{{--                >--}}
-{{--                </admin-products-table-component>--}}
-{{--            </div>--}}
-{{--            <div class="col-md-4">--}}
-{{--                <admin-product-component></admin-product-component>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
     <admin-products-parent
             route-index="{{route('admin.products.index')}}"
             page="{{isset($_GET['page']) ? $_GET['page'] : 1}}"
