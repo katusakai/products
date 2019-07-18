90d4ca558573bb33fde13cdf5e8275ba89989a86
TadasJanca <tadasjanca@gmail.com>
2019-07-18 16:02:14 +0300

    uploaded pictures are now shown

--- app/Aggregators/Products.php
+++ app/Aggregators/Products.php
@@ -5,6 +5,7 @@ namespace App\Aggregators;
 
 
 use App\Config;
+use App\Helpers\UrlParser;
 use App\Product;
 
 class Products
@@ -40,9 +41,19 @@ class Products
         $product['discount'] = $discount;  //todo fix this. in php it dumps integer, but in json it dumps whole object from discount_table
         $product['discounted_price'] = round($this->priceCalculator($product['price'], $discount), 2);
         $this->taxesText($product);
-        $product['images'] = $product->images;
+        $product['images'] = $this->imgUrls($product->images);
 
         return $product;
+    }
+
+    private function imgUrls($images)
+    {
+        foreach ($images as $image) {
+            if (UrlParser::ifLocal($image->image)) {
+                $image->image = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . str_replace('index.php', '', $_SERVER['SCRIPT_NAME']) . 'storage/' . $image->image;
+            }
+        }
+        return $images;
     }
 
     private function priceCalculator($price, $discount)
